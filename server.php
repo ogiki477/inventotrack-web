642e56336436266dae81bae040c92f8fdbc3f489
ogiki477 <omojick88moses@gmail.com>
2024-08-05 16:15:30 +0300

    updated both index and server files

--- server.php
+++ server.php
@@ -1,19 +1,20 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-$uri = urldecode($uri);
+define('LARAVEL_START', microtime(true));
 
-$paths = require __DIR__.'/bootstrap/paths.php';
-
-$requested = $paths['public'].$uri;
-
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
-if ($uri !== '/' and file_exists($requested))
-{
-    return false;
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-require_once $paths['public'].'/index.php';
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
